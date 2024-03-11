2f8f3dc91308fb060aa47ebee899d5fdafee1111
dlwjddn123 <81271328+dlwjddn123@users.noreply.github.com>
2024-03-11 20:48:36 +0900

    [Silver I] Title: 회의실 배정, Time: 440 ms, Memory: 144752 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+I = []
+for _ in range(N):
+    n = list(map(int, input().split()))
+    I.append(n)
+
+I.sort(key=lambda x:(x[0], x[1]))
+result = 1
+present = [I[0]]
+for i in range(1,N):
+    if present[0][1] > I[i][1]:
+        present[0] = I[i]
+    elif present[0][1] <= I[i][0]:
+        present[0] = I[i]
+        result += 1
+print(result)
+        
+
