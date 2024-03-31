6b304f69c0edafcc3069da4c687fd8985a24b439
dlwjddn123 <81271328+dlwjddn123@users.noreply.github.com>
2024-03-31 14:47:15 +0900

    [level 2] Title: 호텔 대실, Time: 2.54 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651#) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 265.21 ms
+메모리: 10.6 MB, 시간: 2.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 31일 14:41:32
+2024년 03월 31일 14:47:13
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,31 +1,19 @@
-from collections import deque
-import heapq
+from heapq import heappop, heappush
 
 def solution(book_time):
-    answer = 0
-    end_times = []
-    
-    for i in range(len(book_time)):
-        start, end = book_time[i][0].replace(":", ""), book_time[i][1].replace(":", "")
-        book_time[i][0], book_time[i][1] = int(start), int(end)
-    book_time.sort(key=lambda x : x[0])
+    answer = 1
+    book_time_ref = [(int(s[:2]) * 60 + int(s[3:]), int(e[:2]) * 60 + int(e[3:])) for s, e in book_time]
+    book_time_ref.sort()
 
-    for i in range(len(book_time)):
-        if len(end_times) == 0:
-            heapq.heappush(end_times, book_time[i][1])
+    heap = []
+    for s, e in book_time_ref:
+        if not heap:
+            heappush(heap,e)
+            continue
+        if heap[0] <= s:
+            heappop(heap)
         else:
-            for _ in range(len(end_times)):
-                t = heapq.heappop(end_times)
-                h, m = t // 100, t % 100
-                m += 10
-                if m >= 60:
-                    h += 1
-                    m -= 60
-                if h < book_time[i][0] // 100 or (h == book_time[i][0] // 100 and m <= book_time[i][0] % 100):
-                    continue
-                heapq.heappush(end_times, t)
-            heapq.heappush(end_times, book_time[i][1])
-        if answer < len(end_times):
-            answer = len(end_times)
-    
+            answer += 1
+        heappush(heap,e+10)
+
     return answer
