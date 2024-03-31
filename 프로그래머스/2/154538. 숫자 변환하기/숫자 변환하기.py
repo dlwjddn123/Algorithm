5fd5df5322cc185bfa63f60c24caf9ac61c96328
dlwjddn123 <81271328+dlwjddn123@users.noreply.github.com>
2024-03-31 15:02:49 +0900

    [level 2] Title: 숫자 변환하기, Time: 30.71 ms, Memory: 17.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def solution(x, y, n):
+    if x == y:
+        return 0
+    
+    answer = find(x, y, n)
+
+    return answer
+
+def find(x, y, n):
+    visited = [False for _ in range(y+1)]
+    queue = deque()
+    queue.append([x, 1])
+    
+    while queue:
+        x, count = queue.popleft()
+        temp = [x * 3, x * 2, x + n]
+        
+        for nx in temp:
+            if nx == y:
+                return count
+            elif nx < y and not visited[nx]:
+                visited[nx] = True
+                queue.append([nx, count + 1])
+    
+    return -1
+                
+            
+        
+            
+    
