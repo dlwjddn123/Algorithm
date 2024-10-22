2d41c16418266a794f92708e21b9c1a9139f1044
dlwjddn123 <81271328+dlwjddn123@users.noreply.github.com>
2024-10-22 15:13:10 +0900

    [Silver IV] Title: 스택, Time: 268 ms, Memory: 20684 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -3,38 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         List<Integer> stack = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            String command = st.nextToken();
-            if (command.equals("push")) {
-                stack.add(Integer.parseInt(st.nextToken()));
-            } else if (command.equals("top")) {
-                if (stack.isEmpty()) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(stack.get(stack.size()-1));
-                }
-            } else if (command.equals("size")) {
-                System.out.println(stack.size());
-            } else if (command.equals("empty")) {
-                if (stack.isEmpty()) {
-                    System.out.println(1);
-                } else {
-                    System.out.println(0);
-                }
-            } else if (command.equals("pop")) {
-                if (stack.isEmpty()) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(stack.remove(stack.size() - 1));
-                }
+            String[] input = br.readLine().split(" ");
+            switch (input[0]) {
+                case "push":
+                    stack.add(Integer.parseInt(input[1]));
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.get(stack.size() - 1));
+                        stack.remove(stack.size() - 1);
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    System.out.println((stack.isEmpty()) ? 1 : 0);
+                    break;
+                case "top":
+                    System.out.println((stack.isEmpty()) ? -1 : stack.get(stack.size() - 1));
             }
         }
     }
